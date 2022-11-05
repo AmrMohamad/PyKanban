f0baa02f3cb3b04d4f19f30442eeafbfb617b229
AmrMohamad <amr.mohamed124124@gmail.com>
2022-11-05 15:55:26 +0200

    set a limit for characters per line inside the card

--- pykanban.py
+++ pykanban.py
@@ -2,29 +2,46 @@ from tabulate import tabulate
 
 
 def card(*sentences) -> str:
-    width_of_card :int = 0
-    for i in range(len(sentences) - 1) : 
-        """Determine the width of card 
-           denpends on longest sentence
-           in sentences
-        """
-        if len(sentences[i+1]) > len(sentences[i]):
-            width_of_card = len(sentences[i+1])
-        else :
-            width_of_card = len(sentences[i])
-    blank_line : str = "| " + (" " * (width_of_card + 2)) + "#" +"\n"
-    top_line : str = " " + ("#" * (width_of_card + 2)) + "#" + "\n"
-    text_line : str = ""
+    width_of_card: int = 40
+    blank_line: str = "| " + (" " * (width_of_card + 2)) + "#" + "\n"
+    top_line: str = " " + ("#" * (width_of_card + 2)) + "#" + "\n"
+    text_line: str = ""
     for sentence in sentences:
-        text_line += "| " + (sentence + " " * (width_of_card - len(sentence))) + "  #" + "\n"
-    buttom_line : str = " " + ("-" * (width_of_card + 2))
-    return f'{top_line}{blank_line}{text_line}{blank_line}{buttom_line}'
+        lines_per_sentence = [""]
+        num_of_sentence_chars = len(sentence)
+        index_line = 0
+        start_line = 0
+        end_line = 32
+        if 0 < num_of_sentence_chars <= 192:
+            num_of_lines = int(round(num_of_sentence_chars / 32))
+            while index_line in range(num_of_lines):
+                lines_per_sentence[index_line] = sentence[start_line:end_line]
+                lines_per_sentence.append("")
+                start_line = end_line
+                end_line += 32
+                index_line += 1
+        else:
+            raise ValueError("maximum number of characters is 192")
+        for i in range(len(lines_per_sentence) - 1):
+            text_line += (
+                "| "
+                + (
+                    lines_per_sentence[i]
+                    + " " * (width_of_card - len(lines_per_sentence[i]))
+                )
+                + "  #"
+                + "\n"
+            )
+    buttom_line: str = " " + ("-" * (width_of_card + 2))
+    return f"{top_line}{blank_line}{text_line}{blank_line}{buttom_line}"
+
+
 
 title_of_card = input("Title: ")
-discription_of_card = input("Discription: ")
 
-print(card(title_of_card,discription_of_card))
-print("\n")
+print(card(title_of_card))
+"""
+discription_of_card = input("Discription: ")
 
 table = [
     ["c1","c2","c3"],
@@ -32,3 +49,4 @@ table = [
     [card(title_of_card,discription_of_card),card(title_of_card,discription_of_card),card(title_of_card,discription_of_card)]
 ]
 print(tabulate(table, tablefmt="double_grid", stralign="center", colalign=("center",)))
+"""
